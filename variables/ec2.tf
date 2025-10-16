dd0edecdfd0f4193f30b50013ecdce26a8a254aa
ravisankar666 <ravisankarp666@gmail.com>
2025-10-16 22:51:54 +0530

    some message

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform" {
     ami = var.ami_id
     instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.allow_alls.id]
-    tags = var.ec2-tags
+    tags = var.ec2_tags
 }
 
 resource "aws_security_group" "allow_alls" {
